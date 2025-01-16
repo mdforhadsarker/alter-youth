53900b8329801adc7179297d5f6e6d84d0885030
Forhad Sarkar <ashadullahalforhad@gmail.com>
2025-01-16 10:58:50 +0600

    document header and title done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" type="image/svg+xml" href="/icon.svg" />
+        <title>AlterYouth | Towards a Literate Bangladesh</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
